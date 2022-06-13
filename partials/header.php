288dbcc268a3f084e34241b995030aa7c737ea1d
xjdevx <xjergx.dev@gmail.com>
2022-06-13 15:01:58 -0400

    first commit

---
+++ partials/header.php
@@ -0,0 +1,20 @@
+<?php 
+// require '../app/Globals.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title><?= APP_NAME ?> </title>
+    <link rel="stylesheet" href="assets/vendors/iconfonts/font-awesome/css/all.min.css">
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.0/css/all.css"> -->
+    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.addons.css">
+    <link rel="stylesheet" href="assets/vendors/waitMe/waitMe.min.css">
+    <link rel="stylesheet" href="includes/flexdatalist/jquery.flexdatalist.min.css">
+    <link rel="stylesheet" type="text/css" href="includes/dataTables/datatables.min.css" />
+    <link rel="stylesheet" href="includes/jqueryui/jquery-ui.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+   
